59785e5bd80c4c8f295f61951c71f84b21b655b2
Audrey <keozon@gmail.com>
2023-10-02 13:22:09 +0200

    finished

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useLikeContext } from '../context/LikeContext';
 
 function Favorites() {
+  const navigate = useNavigate();
   const { likedProducts, toggleLike } = useLikeContext();
   const [likesData, setLikesData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -29,7 +30,7 @@ function Favorites() {
       }
     };
 
-    if (likedProducts.length > 0) {
+    if (likedProducts.length >= 0) {
       fetchLikesData();
     } else {
       setIsLoading(false);
@@ -41,27 +42,46 @@ function Favorites() {
       {isLoading && <div>Products are loading...</div>}
       {error && <div>Error: {error.message}</div>}
       {likesData && (
-        <div>
-          <h1>Liked Products</h1>
-          <ul>
-            {likesData.map((product) => (
-              <li key={product.id}>
-                <i
-                  className={`bi ${
-                    likedProducts.includes(product.id)
-                      ? 'bi-heart-fill'
-                      : 'bi-heart'
-                  } bi-lg position-absolute top-0 end-0 m-2`}
-                  onClick={() => toggleLike(product.id)}
-                ></i>
-                {/* <img src={product.image} alt={product.title} /> */}
-                {product.title} - {product.price}
-                <button>
-                  <Link to={`/product/${product.id}`}>View Details</Link>
-                </button>
-              </li>
-            ))}
-          </ul>
+        <div className='container justify-content-center'>
+          <button
+            className='btn btn-outline-secondary mt-3'
+            onClick={() => navigate(-1)}
+          >
+            &lt;&lt; &nbsp;Go Back
+          </button>
+          <div className='container text-center'>
+            <br />
+
+            <div className='row align-content-center'>
+              {likesData.map((product) => (
+                <div key={product.id} className='col-12 col-md-6 col-lg-3 mb-4'>
+                  <div className='card h-100'>
+                    <div className='card-body d-flex align-items-center justify-content-center'>
+                      <i
+                        className={`bi ${
+                          likedProducts.includes(product.id)
+                            ? 'bi-heart-fill'
+                            : 'bi-heart'
+                        } bi-lg position-absolute top-0 end-0 m-2`}
+                        onClick={() => toggleLike(product.id)}
+                      ></i>
+                      <img
+                        src={product.image}
+                        alt={product.title}
+                        className='card-img-top'
+                      />
+                    </div>
+                    <div className='card-footer d-flex flex-column mt-auto'>
+                      <Link to={`/product/${product.id}`}>
+                        <h5 className='card-title'>{product.title}</h5>
+                      </Link>
+                      <p className='card-text'>Price: ${product.price}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       )}
     </div>
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -17,17 +17,44 @@ function ProductItem() {
       {isLoading && <div>Product is loading...</div>}
       {error && <div>Error: {error.message}</div>}
       {data && (
-        <div>
-          <button onClick={() => navigate(-1)}>Back</button>
-          <i
-            className={`bi ${
-              likedProducts.includes(data.id) ? 'bi-heart-fill' : 'bi-heart'
-            } bi-lg position-absolute top-0 end-0 m-2`}
-            onClick={() => toggleLike(data.id)}
-          ></i>
-          <h3>{data.title}</h3>
-          <p>{data.description}</p>
-          <p>Price: {data.price}</p>
+        <div className='container justify-content-center'>
+          <button
+            className='btn btn-outline-secondary mt-3'
+            onClick={() => navigate(-1)}
+          >
+            &lt;&lt; &nbsp;Go Back
+          </button>
+          <div className='card m-3'>
+            <div className='row g-0 p-3'>
+              <div className='col-md-4 d-flex align-items-center'>
+                <i
+                  className={`bi ${
+                    likedProducts.includes(data.id)
+                      ? 'bi-heart-fill'
+                      : 'bi-heart'
+                  } bi-lg position-absolute top-0 end-0 m-2`}
+                  onClick={() => toggleLike(data.id)}
+                ></i>
+                <img
+                  src={data.image}
+                  className='card-img-top'
+                  alt={data.title}
+                  style={{ maxWidth: '540px' }}
+                />
+              </div>
+              <div className='info col-md-8 d-flex align-items-center'>
+                <div className='card-body d-flex info pl-5'>
+                  <div>{`Category: ${data.category}`}</div>
+                  <h5 className='card-title'>{data.title}</h5>
+                  <p className='card-text description'>{data.description}</p>
+                  <p className='card-text'>{`$${data.price}`}</p>
+                  <div className='card-footer'>
+                    <small className='text-body-secondary'>{`Rating: ${data.rating.rate} from ${data.rating.count}`}</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </div>
